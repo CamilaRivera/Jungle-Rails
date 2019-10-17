c8773fc2a5a3ecb613660d2559188b3dd0bf5da1
CamilaRivera <camila.riverart@gmail.com>
2019-10-17 06:20:55 +0000

    Admin dashboard done

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @categories = Category.all.map{ |category| { name: category.name, count: Product.where(category_id: category.id).count }}
+    @categories_total = Category.all.length
+
+    @products = Product.all.order(quantity: :desc).map{ |product| { name: product.name, count: product.quantity }}
+    @products_quantity = @products.map{ |hash| hash[:count]}.sum
+    @products_total = Product.all.length
+
   end
 end
